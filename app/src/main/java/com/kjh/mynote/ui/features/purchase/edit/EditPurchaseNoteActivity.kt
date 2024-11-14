128e222ac60382894ac8d16df8091931aa769b68
jonghyukK <saz300@naver.com>
2024-11-14 11:11:31 +0900

    fix: 구매노트 작성, 수정시 가격 1일 경우 BottomButton enable 안되는 현상 수정

--- app/src/main/java/com/kjh/mynote/ui/features/purchase/edit/EditPurchaseNoteActivity.kt
+++ app/src/main/java/com/kjh/mynote/ui/features/purchase/edit/EditPurchaseNoteActivity.kt
@@ -30,6 +30,7 @@ import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
+import timber.log.Timber
 import java.time.ZoneOffset
 
 /**
@@ -229,6 +230,12 @@ class EditPurchaseNoteActivity: BaseActivity<ActivityEditOrMakePurchaseNoteBindi
         override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
         override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
         override fun afterTextChanged(s: Editable?) {
+            if (s.toString().isEmpty()) {
+                current = ""
+                viewModel.clearPurchasePrice()
+                return
+            }
+
             if (s.toString() != current) {
                 binding.etPurchasePrice.removeMyTextWatcher()
 
--- app/src/main/java/com/kjh/mynote/ui/features/purchase/make/MakePurchaseNoteActivity.kt
+++ app/src/main/java/com/kjh/mynote/ui/features/purchase/make/MakePurchaseNoteActivity.kt
@@ -210,6 +210,12 @@ class MakePurchaseNoteActivity: BaseActivity<ActivityEditOrMakePurchaseNoteBindi
         override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
         override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
         override fun afterTextChanged(s: Editable?) {
+            if (s.toString().isEmpty()) {
+                current = ""
+                viewModel.clearPurchasePrice()
+                return
+            }
+
             if (s.toString() != current) {
                 binding.etPurchasePrice.removeMyTextWatcher()
 
