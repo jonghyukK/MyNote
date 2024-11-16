b8e184b0592ed35a29bc29ca4d52a0657f9e1666
jonghyukK <saz300@naver.com>
2024-11-16 10:28:56 +0900

    update: BaseDialogFragment내 DialogType 추가
    - BaseDialogFragment에 FullScreen, Dialog Type 추가.
    - theme에 FullScreen Dialog style 추가.
    - DialogType에 따른 다이어로그형, 풀스크린형 로직 추가.

--- app/src/main/java/com/kjh/mynote/ui/base/BaseDialogFragment.kt
+++ app/src/main/java/com/kjh/mynote/ui/base/BaseDialogFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.WindowManager
 import androidx.fragment.app.DialogFragment
 import androidx.viewbinding.ViewBinding
 import com.kjh.mynote.R
@@ -16,8 +15,14 @@ import com.kjh.mynote.utils.extensions.getDisplaySize
  * Description:
  */
 
+enum class DialogType {
+    FULL_SCREEN,
+    DIALOG
+}
+
 abstract class BaseDialogFragment<B: ViewBinding>(
-    private val bindingFactory: (LayoutInflater) -> B
+    private val bindingFactory: (LayoutInflater) -> B,
+    private val dialogType: DialogType = DialogType.DIALOG
 ): DialogFragment() {
 
     private var _binding: B? = null
@@ -26,7 +31,14 @@ abstract class BaseDialogFragment<B: ViewBinding>(
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setStyle(STYLE_NORMAL, R.style.MyDefaultDialog)
+        when (dialogType) {
+            DialogType.FULL_SCREEN -> {
+                setStyle(STYLE_NORMAL, R.style.FullScreenDialog)
+            }
+            DialogType.DIALOG -> {
+                setStyle(STYLE_NORMAL, R.style.MyDefaultDialog)
+            }
+        }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -41,20 +53,29 @@ abstract class BaseDialogFragment<B: ViewBinding>(
         onInitData()
     }
 
-    override fun onResume() {
-        super.onResume()
-
-        activity?.let {
-            val deviceWidth = it.getDisplaySize().width()
-            val params: ViewGroup.LayoutParams? = dialog?.window?.attributes
-            params?.width = deviceWidth - it.resources.getDimensionPixelOffset(R.dimen.dialog_side_space)
-            dialog?.window?.attributes = params as WindowManager.LayoutParams
-        }
+    override fun onStart() {
+        super.onStart()
+        adjustViewWidthWhenTypeIsDialog()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun adjustViewWidthWhenTypeIsDialog() {
+        if (dialogType == DialogType.DIALOG) {
+            activity?.let {
+                val deviceWidth = it.getDisplaySize().width()
+                val sideSpace = it.resources.getDimensionPixelOffset(R.dimen.dialog_side_space)
+
+                dialog?.window?.let { window ->
+                    val params = window.attributes
+                    params.width = deviceWidth - sideSpace
+                    window.attributes = params
+                }
+            }
+        }
     }
 
     protected abstract fun onInitView()
