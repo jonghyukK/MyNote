d589e227fd8de3b01a1cad3995b6e12095206efb
jonghyukK <saz300@naver.com>
2024-12-05 23:11:43 +0900

    add: Data 요청에 따른 결과값 상태관리를 위한 ApiResult sealed class 추가

---
+++ domain/src/main/java/com/example/domain/model/ApiResult.kt
@@ -0,0 +1,58 @@
+package com.example.domain.model
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+/**
+ * Created by kangjonghyuk.
+ * Created On 2024. 12. 5..
+ * Description:
+ */
+
+sealed class ApiResult<out T> {
+    data object Loading: ApiResult<Nothing>()
+    data class Success<T>(val data: T) : ApiResult<T>()
+    data class Error<T>(val error: Throwable) : ApiResult<T>()
+}
+
+inline fun <T, R> ApiResult<T>.getResult(
+    loading: (ApiResult.Loading) -> R,
+    success: (ApiResult.Success<T>) -> R,
+    error: (ApiResult.Error<T>) -> R
+): R = when (this) {
+    is ApiResult.Loading -> loading(this)
+    is ApiResult.Success -> success(this)
+    is ApiResult.Error -> error(this)
+}
+
+inline fun <T> ApiResult<T>.onLoading(
+    action: () -> Unit
+): ApiResult<T> {
+    if (this is ApiResult.Loading) action()
+    return this
+}
+
+inline fun <T> ApiResult<T>.onSuccess(
+    action: (T) -> Unit
+): ApiResult<T> {
+    if (this is ApiResult.Success) action(data)
+    return this
+}
+
+inline fun <T> ApiResult<T>.onError(
+    action: (Throwable) -> Unit
+): ApiResult<T> {
+    if (this is ApiResult.Error) action(error)
+    return this
+}
+
+fun <T> safeApiCall(call: suspend () -> T): Flow<ApiResult<T>> = flow {
+    emit(ApiResult.Loading)
+
+    try {
+        val data = call()
+        emit(ApiResult.Success(data))
+    } catch (e: Exception) {
+        emit(ApiResult.Error(e))
+    }
+}
