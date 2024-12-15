0c6921055e7d6d8d1b453d172c5580ee0e4d46f1
jonghyukK <saz300@naver.com>
2024-12-15 23:53:54 +0900

    add: 카테고리별 구매노트 통계 화면 추가
    - CategoryPurchaseNoteStatsActivity, ViewModel 추가.
    - 화면 UI 생성.

---
+++ app/src/main/java/com/kjh/mynote/ui/features/category/purchasestats/CategoryPurchaseNoteStatsActivity.kt
@@ -0,0 +1,23 @@
+package com.kjh.mynote.ui.features.category.purchasestats
+
+import com.kjh.mynote.databinding.ActivityCategoryPurchaseNoteStatsBinding
+import com.kjh.mynote.ui.base.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+/**
+ * Created by kangjonghyuk.
+ * Created On 2024. 12. 14..
+ * Description:
+ */
+
+@AndroidEntryPoint
+class CategoryPurchaseNoteStatsActivity: BaseActivity<ActivityCategoryPurchaseNoteStatsBinding>({ ActivityCategoryPurchaseNoteStatsBinding.inflate(it) }) {
+
+    override fun onInitView() {
+
+    }
+
+    override fun onInitUiData() {
+
+    }
+}
