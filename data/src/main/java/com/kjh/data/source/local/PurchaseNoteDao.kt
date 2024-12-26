44bb848624cb5e767254f5cf9458dd18a9eeab16
jonghyukK <saz300@naver.com>
2024-12-26 14:42:31 +0900

    update: PurchaseNoteDao 함수 추가 및 수정
    - 구매노트 총 갯수, 총 가격 조회 쿼리 함수 추가.
    - 특정 카테고리의 구매명 목록 및 카운트 조회 함수 추가.
    - 카테고리 목록 및 목록별 구매노트 총 갯수, 총 가격 조회 함수 추가.
    - 구매노트 목록 필터 조회 함수 리턴 타입 변경(oneShot -> Flow)

--- data/src/main/java/com/kjh/data/source/local/PurchaseNoteDao.kt
+++ data/src/main/java/com/kjh/data/source/local/PurchaseNoteDao.kt
@@ -7,6 +7,8 @@ import androidx.room.Query
 import androidx.room.Transaction
 import com.example.domain.model.CategoryStats
 import com.example.domain.model.PurchaseNameStats
+import com.example.domain.model.SortType
+import com.kjh.data.model.dto.PurchaseNoteTotalStatsDto
 import com.kjh.data.model.entity.PurchaseNoteEntity
 import com.kjh.data.model.entity.PurchaseNoteWithCategoryEntity
 import kotlinx.coroutines.flow.Flow
@@ -20,11 +22,16 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface PurchaseNoteDao {
 
+    /**
+     * 구매노트 목록 전체 조회.
+     *
+     * @return Flow<List<PurchaseNoteEntity>>
+     */
     @Query("SELECT * FROM purchase ORDER BY purchaseDate DESC")
-    fun observeAll(): Flow<List<PurchaseNoteEntity>>
+    fun observeAllPurchaseNotes(): Flow<List<PurchaseNoteEntity>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insert(purchaseNoteEntity: PurchaseNoteEntity): Long
+    suspend fun insertPurchaseNote(purchaseNoteEntity: PurchaseNoteEntity): Long
 
     @Transaction
     @Query("SELECT * FROM purchase WHERE id = :id")
@@ -59,16 +66,16 @@ interface PurchaseNoteDao {
         CASE WHEN :sortType = 'HIGH_PRICE' THEN purchasePrice END DESC,
         CASE WHEN :sortType = 'LOW_PRICE' THEN purchasePrice END ASC
     """)
-    suspend fun getFilteredPurchaseNotes(
-        queryText: String?,
-        startDate: Long?,
-        endDate: Long?,
-        minPrice: Long?,
-        maxPrice: Long?,
-        categoryIds: List<Int>,
-        categoryIdsSize: Int,
-        sortType: String
-    ): List<PurchaseNoteWithCategoryEntity>
+    fun getFilteredPurchaseNotes(
+        queryText: String? = null,
+        startDate: Long? = null,
+        endDate: Long? = null,
+        minPrice: Long? = null,
+        maxPrice: Long? = null,
+        categoryIds: List<Int> = emptyList(),
+        categoryIdsSize: Int = 0,
+        sortType: String = SortType.LATEST.name
+    ): Flow<List<PurchaseNoteWithCategoryEntity>>
 
     @Query("SELECT MAX(purchasePrice) FROM purchase")
     fun getMaxPurchasePrice(): Flow<Long?>
@@ -83,28 +90,14 @@ interface PurchaseNoteDao {
         placeName: String,
     ): List<PurchaseNoteWithCategoryEntity>
 
-    @Query("""
-        SELECT 
-            purchaseName,
-            COUNT(*) AS totalCount,
-            COALESCE(SUM(purchasePrice), 0) AS totalPrice
-        FROM 
-            purchase
-        WHERE 
-            categoryId = :categoryId AND
-            (:startDate IS NULL OR purchaseDate >= :startDate) AND
-            (:endDate IS NULL OR purchaseDate <= :endDate)
-        GROUP BY 
-            purchaseName
-        ORDER BY 
-            totalCount DESC
-    """)
-    suspend fun getPurchaseNameRankings(
-        categoryId: Int,
-        startDate: Long?,
-        endDate: Long?
-    ): List<PurchaseNameStats>
 
+    /**
+     * 카테고리 목록 및 목록별 구매노트 총 갯수, 총 가격 조회.
+     *
+     * @param startDate
+     * @param endDate
+     * @return Flow<List<CategoryStats>>
+     */
     @Query("""
         SELECT
             categories.id AS categoryId,
@@ -122,4 +115,61 @@ interface PurchaseNoteDao {
             categories.id, categories.categoryName
     """)
     fun getCategoryStatsByDate(startDate: Long?, endDate: Long?): Flow<List<CategoryStats>>
+
+    /**
+     * 구매노트 총 갯수, 총 가격 조회.
+     *
+     * @param categoryId null이면 전체를 조회.
+     * @param startDate 시작 날짜 (null이면 조건 무시).
+     * @param endDate 종료 날짜 (null이면 조건 무시).
+     * @return 구매노트 총 갯수, 총 가격
+     */
+    @Query("""
+    SELECT 
+        COUNT(*) AS totalCount,
+        COALESCE(SUM(purchasePrice), 0) AS totalPrice
+    FROM 
+        purchase
+    WHERE 
+        (:categoryId IS NULL OR categoryId = :categoryId) AND
+        (:startDate IS NULL OR purchaseDate >= :startDate) AND
+        (:endDate IS NULL OR purchaseDate <= :endDate)
+""")
+    fun getPurchaseNoteTotalStats(
+        categoryId: Int? = null,
+        startDate: Long?,
+        endDate: Long?
+    ): Flow<PurchaseNoteTotalStatsDto>
+
+    /**
+     * 특정 카테고리의 구매명 목록 및 카운트 조회.
+     *
+     * @param categoryId
+     * @param startDate
+     * @param endDate
+     * @return
+     */
+    @Query("""
+    SELECT 
+        p.purchaseName AS purchaseName,
+        COUNT(*) AS totalCount,
+        COALESCE(SUM(p.purchasePrice), 0) AS totalPrice
+    FROM 
+        purchase p
+    WHERE 
+        (:categoryId IS NULL OR p.categoryId = :categoryId) AND
+        (:startDate IS NULL OR p.purchaseDate >= :startDate) AND
+        (:endDate IS NULL OR p.purchaseDate <= :endDate)
+    GROUP BY 
+        p.purchaseName
+    ORDER BY 
+        totalCount DESC
+""")
+    fun getPurchaseNameStats(
+        categoryId: Int?,
+        startDate: Long?,
+        endDate: Long?
+    ): Flow<List<PurchaseNameStats>>
+
+
 }
