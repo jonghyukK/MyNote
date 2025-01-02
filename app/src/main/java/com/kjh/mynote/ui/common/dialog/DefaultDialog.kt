34b4ca394a5e19caf4ecedc58e5a98b1c2e8229c
jonghyukK <saz300@naver.com>
2025-01-03 00:33:02 +0900

    update: DefaultDialog Ui 및 로직 수정
    - DefaultDialog를 사용처의 조건에 맞게 UI 수정.
    - dismiss Event 추가.

--- app/src/main/java/com/kjh/mynote/ui/common/dialog/DefaultDialog.kt
+++ app/src/main/java/com/kjh/mynote/ui/common/dialog/DefaultDialog.kt
@@ -1,12 +1,17 @@
 package com.kjh.mynote.ui.common.dialog
 
 import android.content.Context
+import android.content.DialogInterface
 import android.os.Bundle
+import androidx.annotation.ColorRes
 import androidx.core.view.isVisible
 import com.kjh.mynote.R
 import com.kjh.mynote.databinding.DialogMyDefaultBinding
 import com.kjh.mynote.ui.base.BaseDialogFragment
+import com.kjh.mynote.ui.base.DialogType
 import com.kjh.mynote.utils.extensions.onThrottleClick
+import com.kjh.mynote.utils.extensions.setTextColorRes
+import timber.log.Timber
 
 /**
  * Created by kangjonghyuk.
@@ -16,12 +21,15 @@ import com.kjh.mynote.utils.extensions.onThrottleClick
  *  일반 대화상자형 Dialog
  */
 class DefaultDialog
-    : BaseDialogFragment<DialogMyDefaultBinding>({ DialogMyDefaultBinding.inflate(it) }) {
+    : BaseDialogFragment<DialogMyDefaultBinding>({ DialogMyDefaultBinding.inflate(it) }, dialogType = DialogType.DIALOG) {
 
     private var eventListener: MyDefaultDialogEventListener? = null
 
-    private var contents: String = ""
-    private var posBtnText: String = ""
+    private var title: String? = null
+    private var contents: String? = null
+    private var description: String? = null
+    private var descColorRes: Int = R.color.black_600
+    private var posBtnText: String? = null
     private var negBtnText: String? = null
 
     override fun onAttach(context: Context) {
@@ -36,36 +44,66 @@ class DefaultDialog
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            contents = it.getString(ARG_CONTENTS) ?: ""
-            posBtnText = it.getString(ARG_POS_BTN_TEXT) ?: getString(R.string.confirm)
+            title = it.getString(ARG_STR_TITLE)
+            contents = it.getString(ARG_STR_CONTENTS)
+            description = it.getString(ARG_STR_DESC)
+            descColorRes = it.getInt(ARG_INT_DESC_COLOR)
+            posBtnText = it.getString(ARG_POS_BTN_TEXT)
             negBtnText = it.getString(ARG_NEG_BTN_TEXT)
         }
     }
 
     override fun onInitView() {
-        with (binding) {
-            tvContents.text = contents
+        setupTitle()
+        setupContents()
+        setupDescription()
+        setupPositiveBtnText()
+        setupNegativeBtnText()
+    }
 
-            tvPositive.apply {
-                text = posBtnText
-                onThrottleClick {
-                    eventListener?.onClickPositive()
-                    dismiss()
-                }
+    private fun setupTitle() = with (binding) {
+        tvTitle.isVisible = !title.isNullOrBlank()
+        tvTitle.text = title
+    }
+
+    private fun setupContents() = with (binding) {
+        tvContents.isVisible = !contents.isNullOrBlank()
+        tvContents.text = contents
+    }
+
+    private fun setupDescription() = with (binding) {
+        tvDesc.isVisible = !description.isNullOrBlank()
+        tvDesc.text = description
+        tvDesc.setTextColorRes(descColorRes)
+    }
+
+    private fun setupPositiveBtnText() = with (binding) {
+        tvPositive.apply {
+            text = posBtnText ?: getString(R.string.confirm)
+            onThrottleClick {
+                eventListener?.onDialogPositiveClick()
+                dismiss()
             }
+        }
+    }
 
-            tvNegative.apply {
-                isVisible = !negBtnText.isNullOrEmpty()
-                text = negBtnText ?: ""
-                onThrottleClick {
-                    eventListener?.onClickNegative()
-                    dismiss()
-                }
+    private fun setupNegativeBtnText() = with (binding) {
+        tvNegative.apply {
+            isVisible = !negBtnText.isNullOrEmpty()
+            text = negBtnText
+            onThrottleClick {
+                eventListener?.onDialogNegativeClick()
+                dismiss()
             }
         }
     }
 
     override fun onInitData() {}
+
+    override fun onDismiss(dialog: DialogInterface) {
+        super.onDismiss(dialog)
+        eventListener?.onDialogDismiss()
+    }
 
     override fun onDestroy() {
         super.onDestroy()
@@ -73,24 +111,34 @@ class DefaultDialog
     }
 
     interface MyDefaultDialogEventListener {
-        fun onClickPositive()
-        fun onClickNegative()
+        fun onDialogPositiveClick()
+        fun onDialogNegativeClick()
+        fun onDialogDismiss()
     }
 
     companion object {
         const val TAG = "MyDefaultDialog"
 
-        private const val ARG_CONTENTS = "ARG_CONTENTS"
+        private const val ARG_STR_TITLE = "ARG_STR_TITLE"
+        private const val ARG_STR_CONTENTS = "ARG_STR_CONTENTS"
+        private const val ARG_STR_DESC = "ARG_STR_DESC"
+        private const val ARG_INT_DESC_COLOR = "ARG_INT_DESC_COLOR"
         private const val ARG_POS_BTN_TEXT = "ARG_POS_BTN_TEXT"
         private const val ARG_NEG_BTN_TEXT = "ARG_NEG_BTN_TEXT"
 
         fun newInstance(
-            contents: String,
-            posBtnText: String = "",
-            negBtnText: String = ""
+            title: String? = null,
+            contents: String? = null,
+            desc: String? = null,
+            @ColorRes descColorRes: Int = R.color.black_600,
+            posBtnText: String? = null,
+            negBtnText: String? = null
         ): DefaultDialog = DefaultDialog().apply {
             arguments = Bundle().apply {
-                putString(ARG_CONTENTS, contents)
+                putString(ARG_STR_TITLE, title)
+                putString(ARG_STR_CONTENTS, contents)
+                putString(ARG_STR_DESC, desc)
+                putInt(ARG_INT_DESC_COLOR, descColorRes)
                 putString(ARG_POS_BTN_TEXT, posBtnText)
                 putString(ARG_NEG_BTN_TEXT, negBtnText)
             }
