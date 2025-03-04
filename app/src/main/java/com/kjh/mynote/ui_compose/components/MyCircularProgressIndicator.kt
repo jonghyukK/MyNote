18406e9258cc569ee39c1bd384b71502ccdd9e0c
jonghyukK <saz300@naver.com>
2025-03-04 15:32:25 +0900

    add: 공통 컴포넌트 MyCircularProgressIndicator 추가
    - Composable Screen 내에서 공통으로 쓰일 로딩 인디케이터 추가.

---
+++ app/src/main/java/com/kjh/mynote/ui_compose/components/MyCircularProgressIndicator.kt
@@ -0,0 +1,25 @@
+package com.kjh.mynote.ui_compose.components
+
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import com.kjh.mynote.R
+
+/**
+ * Created by kangjonghyuk.
+ * Created On 2025. 3. 4..
+ * Description:
+ */
+
+@Composable
+fun MyCircularProgressIndicator(
+    modifier: Modifier = Modifier
+) {
+    CircularProgressIndicator(
+        modifier = modifier.width(46.dp),
+        color = colorResource(R.color.colorPrimary)
+    )
+}
