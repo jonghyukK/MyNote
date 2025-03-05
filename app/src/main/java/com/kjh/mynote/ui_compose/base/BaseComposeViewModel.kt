696b7d6612337d3a0a2cb1ee7004bfaeef6a6b67
jonghyukK <saz300@naver.com>
2025-03-06 04:03:06 +0900

    Update: BaseComposeViewModel 수정 (테스트)
    1. 기존 setState 함수를 이용해 state 변경 처리하던 부분을
    reduceState 추상 함수 추가 후, 순수함수 리듀서 기능 추가.

--- app/src/main/java/com/kjh/mynote/ui_compose/base/BaseComposeViewModel.kt
+++ app/src/main/java/com/kjh/mynote/ui_compose/base/BaseComposeViewModel.kt
@@ -5,9 +5,13 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.runningFold
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 /**
@@ -22,44 +26,21 @@ interface UiEvent
 
 interface UiSideEffect
 
-abstract class BaseComposeViewModel<E: UiEvent, S: UiState, SE: UiSideEffect>: ViewModel() {
+abstract class BaseComposeViewModel<E: UiEvent, S: UiState, SE: UiSideEffect>(defaultState: S): ViewModel() {
 
-    private val initialState: S by lazy { createInitialState() }
-    abstract fun createInitialState(): S
+    private val event = Channel<E>()
 
-    val currentState: S
-        get() = uiState.value
+    val state: StateFlow<S> = event.receiveAsFlow()
+        .runningFold(defaultState, ::reduceState)
+        .stateIn(viewModelScope, SharingStarted.Eagerly, defaultState)
 
-    private val _uiState: MutableStateFlow<S> = MutableStateFlow(initialState)
-    val uiState = _uiState.asStateFlow()
-
-    private val _event: MutableSharedFlow<E> = MutableSharedFlow()
-    val event = _event.asSharedFlow()
+    protected abstract suspend fun reduceState(current: S, event: E): S
 
     private val _effect: Channel<SE> = Channel()
     val effect = _effect.receiveAsFlow()
 
-    init {
-        subscribeEvents()
-    }
-
-    private fun subscribeEvents() {
-        viewModelScope.launch {
-            event.collect {
-                handleEvent(it)
-            }
-        }
-    }
-
-    abstract fun handleEvent(event: E)
-
     fun setEvent(event: E) {
-        viewModelScope.launch { _event.emit(event) }
-    }
-
-    protected fun setState(reduce: S.() -> S) {
-        val newState = currentState.reduce()
-        _uiState.value = newState
+        viewModelScope.launch { this@BaseComposeViewModel.event.send(event) }
     }
 
     protected fun setEffect(builder: () -> SE) {
@@ -67,3 +48,56 @@ abstract class BaseComposeViewModel<E: UiEvent, S: UiState, SE: UiSideEffect>: V
         viewModelScope.launch { _effect.send(effectValue) }
     }
 }
+
+
+//interface UiState
+//
+//interface UiEvent
+//
+//interface UiSideEffect
+//
+//abstract class BaseComposeViewModel<E: UiEvent, S: UiState, SE: UiSideEffect>: ViewModel() {
+//
+//    private val initialState: S by lazy { createInitialState() }
+//    abstract fun createInitialState(): S
+//
+//    val currentState: S
+//        get() = uiState.value
+//
+//    private val _uiState: MutableStateFlow<S> = MutableStateFlow(initialState)
+//    val uiState = _uiState.asStateFlow()
+//
+//    private val _event: MutableSharedFlow<E> = MutableSharedFlow()
+//    val event = _event.asSharedFlow()
+//
+//    private val _effect: Channel<SE> = Channel()
+//    val effect = _effect.receiveAsFlow()
+//
+//    init {
+//        subscribeEvents()
+//    }
+//
+//    private fun subscribeEvents() {
+//        viewModelScope.launch {
+//            event.collect {
+//                handleEvent(it)
+//            }
+//        }
+//    }
+//
+//    abstract fun handleEvent(event: E)
+//
+//    fun setEvent(event: E) {
+//        viewModelScope.launch { _event.emit(event) }
+//    }
+//
+//    protected fun setState(reduce: S.() -> S) {
+//        val newState = currentState.reduce()
+//        _uiState.value = newState
+//    }
+//
+//    protected fun setEffect(builder: () -> SE) {
+//        val effectValue = builder()
+//        viewModelScope.launch { _effect.send(effectValue) }
+//    }
+//}
